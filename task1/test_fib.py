02e17fe2c829624f936d08b8b01af59a9da4165e
jhershaff <jhershaff@gmail.com>
2019-02-01 10:21:10 -0500

    re-saved test files in new directory

--- task1/test_fib.py
+++ task1/test_fib.py
@@ -1,6 +1,7 @@
 from fib import fib
 
 def test_fib():
+    assert fib(1)==1
     assert fib(2)==1
     assert fib(5)==5
     assert fib(12)==144
--- task1/test_inc.py
+++ task1/test_inc.py
@@ -2,4 +2,5 @@ from inc import inc
 
 def test_answer():
   assert inc(3) == 4
+  assert inc(0) == 1
 
